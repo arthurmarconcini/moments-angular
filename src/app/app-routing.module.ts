102cc5aff0af3e424733a37d8ff51593ff6a88cd
Arthur Marconcini <arthur.m.ramos@hotmail.com>
2022-04-12 19:39:03 -0300

    feat: Home components functions and moment search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'moments/new', component: NewMomentComponent },
+
+  { path: 'moments/:id', component: MomentComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'moments/new', component: NewMomentComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
